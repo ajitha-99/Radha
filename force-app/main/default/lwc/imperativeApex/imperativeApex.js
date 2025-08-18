4ac7a1e882551ffbdd9205bfb41a234d205f3d07
ajitha-99 <ajithag9999@gmail.com>
2025-08-18 14:03:16 -0500

    commit

--- force-app/main/default/lwc/imperativeApex/imperativeApex.js
+++ force-app/main/default/lwc/imperativeApex/imperativeApex.js
@@ -1,3 +1,23 @@
 import { LightningElement } from 'lwc';
+import getAccount from '@salesforce/apex/AccountLwc.getAccount';
+export default class ImperativeApex extends LightningElement {
 
-export default class ImperativeApex extends LightningElement {}
+
+clickHandler(){
+
+//we have to make a call to the imperative apex
+//It means you are calling a method from apex and 
+//returning the data or handling the response in the form of promise
+
+getAccount().then(result =>{
+    console.log("Account Records", result)
+})
+ .catch((error) =>{
+ console.log("Account error", error)
+});
+}
+
+
+
+
+}
