e2abb9c8547bd857676c1223822bb97e95759c10
ajitha-99 <ajithag9999@gmail.com>
2025-08-18 18:37:58 -0500

    LWC Static Resources

---
+++ force-app/main/default/lwc/staticResourceDemo/staticResourceDemo.js
@@ -0,0 +1,9 @@
+import { LightningElement } from 'lwc';
+import LOGO from '@salesforce/resourceUrl/MyLogo';    
+import contentasset from '@salesforce/resourceUrl/contentasset';
+export default class StaticResourceDemo extends LightningElement {
+
+mylogoImage = LOGO;
+
+
+}
